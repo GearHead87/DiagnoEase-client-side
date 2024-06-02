dfa5147f1119de28c49787d81e118a301302e41c
GearHead87 <hosanulislam87@gmail.com>
2024-06-02 19:50:30 +0600

    dashboard for admin and user, useAdmin, useRole hooks added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -6,7 +6,7 @@ const Main = () => {
 	return (
 		<div>
 			<Navbar />
-			<div className="pt-24 min-h-[calc(100vh-68px)]">
+			<div className="pt-24 min-h-[calc(100vh-197px)]">
 				<Outlet />
 			</div>
 			<Footer />
