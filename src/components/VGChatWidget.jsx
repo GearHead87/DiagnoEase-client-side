d69f795dc61c9516186c6d11ddba67f6883ebf60
GearHead87 <hosanulislam87@gmail.com>
2025-02-15 00:08:26 +0600

    Fix(chat agent): fix the chat widget

--- src/components/VGChatWidget.jsx
+++ src/components/VGChatWidget.jsx
@@ -1,34 +1,29 @@
 import { useEffect } from 'react';
 
 const VGChatWidget = () => {
-  useEffect(() => {
-    window.VG_CONFIG = {
-      ID: import.meta.env.VITE_CHAT_AGENT_ID,
-      region: import.meta.env.VITE_CHAT_REGION,
-      render: 'bottom-right',
-      stylesheets: [
-        "https://vg-bunny-cdn.b-cdn.net/vg_live_build/styles.css",
-      ],
-    };
+	useEffect(() => {
+		window.VG_CONFIG = {
+			ID: 'fxx7jvn09kdw3s80',
+			region: 'eu',
+			render: 'bottom-right',
+			stylesheets: ['https://vg-bunny-cdn.b-cdn.net/vg_live_build/styles.css'],
+		};
 
-    const script = document.createElement("script");
-    script.src = "https://vg-bunny-cdn.b-cdn.net/vg_live_build/vg_bundle.js";
-    script.defer = true;
-    document.body.appendChild(script);
+		const script = document.createElement('script');
+		script.src = 'https://vg-bunny-cdn.b-cdn.net/vg_live_build/vg_bundle.js';
+		script.defer = true;
+		document.body.appendChild(script);
 
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+		return () => {
+			document.body.removeChild(script);
+		};
+	}, []);
 
-  return (
-    <div 
-      id="VG_OVERLAY_CONTAINER" 
-      style={{ width: 0, height: 0 }}
-    >
-      {/* Here is where TIXAE Agents renders the widget */}
-    </div>
-  );
+	return (
+		<div id="VG_OVERLAY_CONTAINER" style={{ width: 0, height: 0 }}>
+			{/* Here is where TIXAE Agents renders the widget */}
+		</div>
+	);
 };
 
-export default VGChatWidget; 
+export default VGChatWidget;
