810ed1d7d179db069703c2a9b700e0c9ec2d0a31
GearHead87 <hosanulislam87@gmail.com>
2025-02-17 22:52:15 +0600

    Revert "Merge pull request #3 from GearHead87/Feat/AI-Voice"
    This reverts commit 39714e9540b4ff83f54390154047458ee06b6b79, reversing
    changes made to c6605a8f018235c109f105c10b3637dd1b752964.

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import AuthProvider from "./providers/AuthProvider.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { HelmetProvider } from "react-helmet-async";
-import VGChatWidget from "./components/VGChatWidget";
 // import 'flowbite';
 
 const queryClient = new QueryClient();
@@ -18,7 +17,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 				<AuthProvider>
 					<RouterProvider router={router} />
 					<Toaster />
-					<VGChatWidget />
 				</AuthProvider>
 			</QueryClientProvider>
 		</HelmetProvider>
